b837dd0135568a89f5d3f8aaa4d569cb84e383d9
Lara <sabatinilara@gmail.com>
2022-08-26 12:20:34 -0300

    search user test

--- api/services/users.js
+++ api/services/users.js
@@ -17,11 +17,11 @@ async function getMultiple(page = 1){
   }
 }
 
-async function searchUser(name, page = 1){
+async function searchUser(value, page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
     // `SELECT * FROM users WHERE name = '%${name}%' AND password = '%${password}%' LIMIT ${offset},${config.listPerPage}`
-    `SELECT * FROM users WHERE name = '%${name}%' LIMIT ${offset},${config.listPerPage}`
+    `SELECT * FROM users WHERE name = '%${value}%' LIMIT ${offset},${config.listPerPage}`
   )
   const data = helper.emptyOrRows(rows);
   const meta = {page};
