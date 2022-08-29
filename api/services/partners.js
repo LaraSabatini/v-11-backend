2187857d7af9ed6dd764ae18dc98d45ccb22bf04
Lara <sabatinilara@gmail.com>
2022-08-29 15:54:28 -0300

    fixed typos

--- api/services/partners.js
+++ api/services/partners.js
@@ -33,7 +33,7 @@ async function searchPartner(value, page = 1){
 
 async function createPartner(partner){
   const result = await db.query(
-    `INSERT INTO partnerts(name, last_name, identification_number, birth_date, email, membership_start_date, membership_time_paid, payment_expire_date, payment_is_active, created_by, trainer_id, free_pass)
+    `INSERT INTO partners(name, last_name, identification_number, birth_date, email, membership_start_date, membership_time_paid, payment_expire_date, payment_is_active, created_by, trainer_id, free_pass)
     VALUES ('${partner.name}','${partner.last_name}', '${partner.identification_number}', '${partner.birth_date}', '${partner.email}', '${partner.membership_start_date}', '${partner.membership_time_paid}', '${partner.payment_expire_date}', '${partner.payment_is_active}, '${partner.created_by}', '${partner.trainer_id}', '${partner.free_pass}')`
   );
   let message = 'Error in creating partner';
