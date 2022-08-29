04ff8a1398948c3e6d479115419b246e0957f198
Lara <sabatinilara@gmail.com>
2022-08-29 15:52:20 -0300

    test

--- api/routes/partnerts.js
+++ api/routes/partnerts.js
@@ -14,24 +14,24 @@ router.get('/', async function(req, res, next) {
 });
 
 /* SEARCH partners */
-// router.get('/:value', async function(req, res, next) {
-//     try {
-//       res.json(await partners.searchPartner(req.params.value, req.query.page));
-//       console.log(req);
-//     } catch (err) {
-//       console.error(`Error while getting search `, err.message);
-//       next(err);
-//     }
-// });
+router.get('/:value', async function(req, res, next) {
+    try {
+      res.json(await partners.searchPartner(req.params.value, req.query.page));
+      console.log(req);
+    } catch (err) {
+      console.error(`Error while getting search `, err.message);
+      next(err);
+    }
+});
 
-// /* POST partners */
-// router.post('/', async function(req, res, next) {
-//   try {
-//     res.json(await partners.create(req.body));
-//   } catch (err) {
-//     console.error(`Error while creating partner`, err.message);
-//     next(err);
-//   }
-// });
+/* POST partners */
+router.post('/', async function(req, res, next) {
+  try {
+    res.json(await partners.create(req.body));
+  } catch (err) {
+    console.error(`Error while creating partners`, err.message);
+    next(err);
+  }
+});
 
 module.exports = router;
--- api/services/partners.js
+++ api/services/partners.js
@@ -15,38 +15,38 @@ async function getMultiple(page = 1){
       data,
       meta
   }
-}
+};
 
-// async function searchPartner(value, page = 1){
-//     const offset = helper.getOffset(page, config.listPerPage);
-//     const rows = await db.query(
-//       `SELECT * FROM partners WHERE name LIKE '%${value}%' LIMIT ${offset},${config.listPerPage}`
-//     )
-//     const data = helper.emptyOrRows(rows);
-//     const meta = {page};
+async function searchPartner(value, page = 1){
+    const offset = helper.getOffset(page, config.listPerPage);
+    const rows = await db.query(
+      `SELECT * FROM partners WHERE name LIKE '%${value}%' LIMIT ${offset},${config.listPerPage}`
+    )
+    const data = helper.emptyOrRows(rows);
+    const meta = {page};
   
-//     return {
-//         data,
-//         meta
-//     }
-// }
+    return {
+        data,
+        meta
+    }
+};
 
-// async function createPartner(partner){
-//   const result = await db.query(
-//     `INSERT INTO partnerts(name, last_name, identification_number, birth_date, email, membership_start_date, membership_time_paid, payment_expire_date, payment_is_active, created_by, trainer_id, free_pass)
-//     VALUES ('${partner.name}','${partner.last_name}', '${partner.identification_number}', '${partner.birth_date}', '${partner.email}', '${partner.membership_start_date}', '${partner.membership_time_paid}', '${partner.payment_expire_date}', '${partner.payment_is_active}, '${partner.created_by}', '${partner.trainer_id}', '${partner.free_pass}')`
-//   );
-//   let message = 'Error in creating partner';
+async function createPartner(partner){
+  const result = await db.query(
+    `INSERT INTO partnerts(name, last_name, identification_number, birth_date, email, membership_start_date, membership_time_paid, payment_expire_date, payment_is_active, created_by, trainer_id, free_pass)
+    VALUES ('${partner.name}','${partner.last_name}', '${partner.identification_number}', '${partner.birth_date}', '${partner.email}', '${partner.membership_start_date}', '${partner.membership_time_paid}', '${partner.payment_expire_date}', '${partner.payment_is_active}, '${partner.created_by}', '${partner.trainer_id}', '${partner.free_pass}')`
+  );
+  let message = 'Error in creating partner';
 
-//   if (result.affectedRows) {
-//     message = 'partner created successfully';
-//   }
+  if (result.affectedRows) {
+    message = 'partner created successfully';
+  }
 
-//   return {message};
-// }
+  return {message};
+};
 
 module.exports = {
   getMultiple,
-  // searchPartner,
-  // createPartner
+  searchPartner,
+  createPartner
 }
