d1a2010a3c0f0799ab53bd4d89d923fc8005b39b
Lara <sabatinilara@gmail.com>
2022-08-31 12:20:18 -0300

    created put endpoint

--- api/routes/partners.js
+++ api/routes/partners.js
@@ -56,4 +56,14 @@ router.post('/', async function(req, res, next) {
   }
 });
 
+/* PUT partner */
+router.put('/:id', async function(req, res, next) {
+  try {
+    res.json(await partners.update(req.params.id, req.body));
+  } catch (err) {
+    console.error(`Error while updating partner`, err.message);
+    next(err);
+  }
+});
+
 module.exports = router;
