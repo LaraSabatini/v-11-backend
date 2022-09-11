f811f1adfad752ef0cfd67fa39f54531993fdbbe
Lara <sabatinilara@gmail.com>
2022-09-11 18:45:28 -0300

    fixed bug

--- api/services/partnersPayment.js
+++ api/services/partnersPayment.js
@@ -33,7 +33,7 @@ async function searchPurchasesByPartner(value, page = 1){
 async function create(partnerPayment){
   const result = await db.query(
     `INSERT INTO partner_payments(partner_id, partner_name, partner_last_name, combo, time_paid, time_paid_unit, clases_paid, trainer_id, trainer_name, payment_method_id, payment_method_name, price_paid, date, payment_expire_date)
-    VALUES ('${partnerPayment.partner_id}','${partnerPayment.partner_name}', '${partnerPayment.partner_last_name}', '${partnerPayment.combo}', '${partnerPayment.time_paid}', '${partnerPayment.time_paid_unit}', '${partnerPayment.clases_paid}', '${partnerPayment.trainer_id}', '${partnerPayment.trainer_name}', '${partnerPayment.payment_method_id}', '${partnerPayment.payment_method_name}', '${partnerPayment.price_paid}', '${partnerPayment.date}', '${partnerPayment.payment_expire_date})`
+    VALUES ('${partnerPayment.partner_id}','${partnerPayment.partner_name}', '${partnerPayment.partner_last_name}', '${partnerPayment.combo}', '${partnerPayment.time_paid}', '${partnerPayment.time_paid_unit}', '${partnerPayment.clases_paid}', '${partnerPayment.trainer_id}', '${partnerPayment.trainer_name}', '${partnerPayment.payment_method_id}', '${partnerPayment.payment_method_name}', '${partnerPayment.price_paid}', '${partnerPayment.date}', '${partnerPayment.payment_expire_date}')`
   );
 
   let message = 'Error in creating partnerPayment';
