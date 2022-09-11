402d0a55a59da9d3b8297ddf432093b49c9ddc9a
Lara <sabatinilara@gmail.com>
2022-09-11 14:17:26 -0300

    changed database

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-    db: {
-      host: "mysql-tester-database.alwaysdata.net",
-      user: "271839_v11",
-      password: "V11ESCALADA!",
-      database: "tester-database_v11",
-    },
-    listPerPage: 25,
+  db: {
+    host: "vonceescalada.com",
+    user: "u611741292_lara_sabatini",
+    password: "v11Escalada!",
+    database: "u611741292_v_11",
+  },
+  listPerPage: 25,
 };
-  
+
 module.exports = config;
