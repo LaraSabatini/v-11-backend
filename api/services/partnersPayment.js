f2dc039b746e9c7d02778ae8c9e1e44e46c3501b
Lara <sabatinilara@gmail.com>
2022-09-15 15:58:02 -0300

    added days and hour key

--- api/services/partnersPayment.js
+++ api/services/partnersPayment.js
@@ -32,8 +32,8 @@ async function searchPurchasesByPartner(value, page = 1){
 
 async function create(partnerPayment){
   const result = await db.query(
-    `INSERT INTO partner_payments(partner_id, partner_name, partner_last_name, combo, time_paid, time_paid_unit, clases_paid, trainer_id, trainer_name, payment_method_id, payment_method_name, price_paid, date, payment_expire_date)
-    VALUES ('${partnerPayment.partner_id}','${partnerPayment.partner_name}', '${partnerPayment.partner_last_name}', '${partnerPayment.combo}', '${partnerPayment.time_paid}', '${partnerPayment.time_paid_unit}', '${partnerPayment.clases_paid}', '${partnerPayment.trainer_id}', '${partnerPayment.trainer_name}', '${partnerPayment.payment_method_id}', '${partnerPayment.payment_method_name}', '${partnerPayment.price_paid}', '${partnerPayment.date}', '${partnerPayment.payment_expire_date}')`
+    `INSERT INTO partner_payments(partner_id, partner_name, partner_last_name, combo, time_paid, time_paid_unit, clases_paid, payment_method_id, payment_method_name, price_paid, date, payment_expire_date, trainer_id, trainer_name, days_and_hours)
+    VALUES ('${partnerPayment.partner_id}','${partnerPayment.partner_name}', '${partnerPayment.partner_last_name}', '${partnerPayment.combo}', '${partnerPayment.time_paid}', '${partnerPayment.time_paid_unit}', '${partnerPayment.clases_paid}', '${partnerPayment.payment_method_id}', '${partnerPayment.payment_method_name}', '${partnerPayment.price_paid}', '${partnerPayment.date}', '${partnerPayment.payment_expire_date}', '${partnerPayment.trainer_id}', '${partnerPayment.trainer_name}', '${partnerPayment.days_and_hours}')`
   );
 
   let message = 'Error in creating partnerPayment';
@@ -47,7 +47,7 @@ async function create(partnerPayment){
 
 async function update(id, partnerPayment){
   const result = await db.query(
-    `UPDATE partner_payments SET id='${partnerPayment.id}',partner_id='${partnerPayment.partner_id}',partner_name='${partnerPayment.partner_name}',partner_last_name='${partnerPayment.partner_last_name}',combo='${partnerPayment.combo}',time_paid='${partnerPayment.time_paid}',time_paid_unit='${partnerPayment.time_paid_unit}',clases_paid='${partnerPayment.clases_paid}', trainer_id='${partnerPayment.trainer_id}', trainer_name='${partnerPayment.trainer_name}', payment_method_id='${partnerPayment.payment_method_id}', payment_method_name='${partnerPayment.payment_method_name}', price_paid='${partnerPayment.price_paid}', date='${partnerPayment.date}', payment_expire_date='${partnerPayment.payment_expire_date}' WHERE id='${id}'`
+    `UPDATE partner_payments SET id='${partnerPayment.id}',partner_id='${partnerPayment.partner_id}',partner_name='${partnerPayment.partner_name}',partner_last_name='${partnerPayment.partner_last_name}',combo='${partnerPayment.combo}',time_paid='${partnerPayment.time_paid}',time_paid_unit='${partnerPayment.time_paid_unit}',clases_paid='${partnerPayment.clases_paid}', trainer_id='${partnerPayment.trainer_id}', trainer_name='${partnerPayment.trainer_name}', payment_method_id='${partnerPayment.payment_method_id}', payment_method_name='${partnerPayment.payment_method_name}', price_paid='${partnerPayment.price_paid}', date='${partnerPayment.date}', payment_expire_date='${partnerPayment.payment_expire_date}', days_and_hours='${partnerPayment.days_and_hours}' WHERE id='${id}'`
   );
 
   let message = 'Error in updating payment';
