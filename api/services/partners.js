d3c156fbc5be4e036efd4d5688a7915145df1006
Lara <sabatinilara@gmail.com>
2022-09-15 16:17:30 -0300

    d;

--- api/services/partners.js
+++ api/services/partners.js
@@ -33,7 +33,7 @@ async function searchPartner(value, page = 1){
 async function filterStudents(value, page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `SELECT * FROM partners WHERE trainer_id > '${value}' LIMIT ${offset},${config.listPerPage}`
+    `SELECT * FROM partners WHERE is_student = '${value}' LIMIT ${offset},${config.listPerPage}`
     )
   const data = helper.emptyOrRows(rows);
   const meta = {page};
@@ -60,8 +60,8 @@ async function filterFreePass(value, page = 1){
 
 async function create(partner){
   const result = await db.query(
-    `INSERT INTO partners(name, last_name, identification_number, birth_date, email, phone, subs, membership_start_date, created_by, trainer_id, free_pass)
-    VALUES ('${partner.name}','${partner.last_name}', '${partner.identification_number}', '${partner.birth_date}', '${partner.email}', '${partner.phone}', '${partner.subs}', '${partner.membership_start_date}', '${partner.created_by}', '${partner.trainer_id}', '${partner.free_pass}')`
+    `INSERT INTO partners(name, last_name, identification_number, birth_date, email, phone, subs, membership_start_date, created_by, free_pass, is_student)
+    VALUES ('${partner.name}','${partner.last_name}', '${partner.identification_number}', '${partner.birth_date}', '${partner.email}', '${partner.phone}', '${partner.subs}', '${partner.membership_start_date}', '${partner.created_by}', '${partner.is_student}', '${partner.free_pass}')`
   );
 
   let message = 'Error in creating partner';
@@ -76,7 +76,7 @@ async function create(partner){
 
 async function update(id, partner){
   const result = await db.query(
-    `UPDATE partners SET id='${partner.id}',name='${partner.name}',last_name='${partner.last_name}',identification_number='${partner.identification_number}',birth_date='${partner.birth_date}',email='${partner.email}',phone='${partner.phone}',subs='${partner.subs}',membership_start_date='${partner.membership_start_date}',created_by='${partner.created_by}',trainer_id='${partner.trainer_id}',free_pass='${partner.free_pass}' WHERE id='${id}'`
+    `UPDATE partners SET id='${partner.id}',name='${partner.name}',last_name='${partner.last_name}',identification_number='${partner.identification_number}',birth_date='${partner.birth_date}',email='${partner.email}',phone='${partner.phone}',subs='${partner.subs}',membership_start_date='${partner.membership_start_date}',created_by='${partner.created_by}',is_student='${partner.is_student}',free_pass='${partner.free_pass}' WHERE id='${id}'`
   );
 
   let message = 'Error in updating partner';
