c0543a0c2ae3d45a7f73fa6f12cd0388aa4465eb
Lara <sabatinilara@gmail.com>
2022-09-23 13:39:05 -0300

    added store payments to index

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const partnersPaymentRouter = require("./api/routes/partnersPayment")
 const productPurchasesRouter = require("./api/routes/productPurchases")
 const scheduleRouter = require("./api/routes/schedule")
 const digitalPaymentsRouter = require("./api/routes/digitalPayments")
+const storePaymentsRouter = require("./api/routes/storePayments")
 
 app.use(express.json());
 app.use(
@@ -133,6 +134,15 @@ app.use((err, req, res, next) => {
 });
 
 app.use("/digitalPayments", digitalPaymentsRouter);
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message});
+  return;
+});
+
+app.use("/storePayments", storePaymentsRouter);
 /* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
