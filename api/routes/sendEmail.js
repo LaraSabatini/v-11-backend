699da7e3be7072d4de387d86a5474348473754d1
Lara <sabatinilara@gmail.com>
2022-11-18 14:19:29 -0300

    created new implementation for email

--- api/routes/sendEmail.js
+++ api/routes/sendEmail.js
@@ -1,17 +1,36 @@
+const nodemailer = require('nodemailer');
 const express = require('express');
 const router = express.Router();
-const emailer = require('../../emailer')
+require('dotenv').config();
 
-router.post('/send-email', async function(req, res, next) {
-    try {
-        emailer.sendMail(req.body.recipients, req.body.subject, req.body.text, req.body.category)
-         res.send({ 
-             info: "Mail sent successfully",
-         })
-    } catch (err) {
-      console.error(`Error while sending email`, err.message);
-      next(err);
+router.post('/', async function(req, res) {
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: 587,
+        secure: false,
+        auth: {
+            user: "info@vonceescalada.com",
+            pass: process.env.MAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: "info@vonceescalada.com",
+        to: req.body.recipients,
+        subject: req.body.subject,
+        text: req.body.text
     }
-  });
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            res.status(500).send(error.message)
+        } else {
+            const response = {
+                status: 200,
+            }
+            res.status(200).json(response)
+        }
+    })
+});
 
 module.exports = router;
