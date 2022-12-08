5410a147234ddf337d0522a915590a419a49f2af
Lara <sabatinilara@gmail.com>
2022-12-08 15:13:57 -0300

    added categories api

--- api/services/categories.js
+++ api/services/categories.js
@@ -1,6 +1,8 @@
 const db = require('./db');
 const helper = require('../../helper');
 const config = require('../../config');
+const successResponses = require('../../strings/successMessages');
+const errorResponses = require('../../strings/errorMessages');
 
 const table = 'categories';
 
@@ -18,6 +20,28 @@ async function getMultiple(page = 1) {
   };
 }
 
+async function create(Category) {
+  const result = await db.query(
+    `INSERT INTO ${table}(name)
+      VALUES ('${Category.name}')`,
+  );
+
+  let message = {
+    status: 500,
+    message: errorResponses.createCategory,
+  };
+
+  if (result.affectedRows) {
+    message = {
+      status: 200,
+      message: successResponses.createCategory,
+    };
+  }
+
+  return { message };
+}
+
 module.exports = {
   getMultiple,
+  create,
 };
